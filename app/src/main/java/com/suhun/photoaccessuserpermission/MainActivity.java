6349fc4371c80b7ad224343aed42678da1e31e00
Shu-Hung Lin <suhunlin0310@gmail.com>
2023-09-14 11:12:37 +0800

    Add code logic to check Manifest.permission.READ_EXTERNAL_STORAGE, if user not agree, send request to user. If user already agree Manifest.permission.READ_EXTERNAL_STORAGE, initial ContentResolver

--- app/src/main/java/com/suhun/photoaccessuserpermission/MainActivity.java
+++ app/src/main/java/com/suhun/photoaccessuserpermission/MainActivity.java
@@ -1,7 +1,11 @@
 package com.suhun.photoaccessuserpermission;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ListView;
@@ -23,6 +27,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initListView();
+        if(checkUserAgreeReadExternalStorage()){
+            initContentResolver();
+        }else{
+            requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 123);
+        }
     }
 
     private void initListView(){
@@ -34,6 +43,30 @@ public class MainActivity extends AppCompatActivity {
         listViewData.put(from[0], "Happy");
         data.add(listViewData);
         simpleAdapter.notifyDataSetChanged();*/
+    }
+
+    private void initContentResolver(){
+
+    }
+
+    private boolean checkUserAgreeReadExternalStorage(){
+        boolean result = false;
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED){
+            result = true;
+        }
+        return result;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode == 123){
+            if(grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                initContentResolver();
+            }else{
+                finish();
+            }
+        }
     }
 
     public void getFilePathFun(View view){
